ab467832004067ebc88391ff2a45d535e25bc9f2
Luix-Dev <luiizperren@hotmail.com>
2023-09-08 00:25:49 +0200

    played some with the stars

--- src/components/ParallaxStarfield.vue
+++ src/components/ParallaxStarfield.vue
@@ -1,45 +1,64 @@
 <template>
   <div id="starfield-container">
-    <!-- Content other than the stars -->
+    <!-- Stars will be generated here -->
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      stars: [],
+    };
+  },
   mounted() {
-    const container = document.getElementById('starfield-container');
-    const numberOfStars = 100;
-    const colors = ['#8921C2', '#FE39A4', '#FFFDBB', '#53E8D4', '#25C4F8'];
+    this.createStars();
+    this.animateStars();
+  },
+  methods: {
+    createStars() {
+      const numberOfStars = 100;
+      const colors = ['#8921C2', '#FE39A4', '#FFFDBB', '#53E8D4', '#25C4F8'];
 
-    for (let i = 0; i < numberOfStars; i++) {
-      const star = document.createElement('div');
-      star.className = 'star';
+      for (let i = 0; i < numberOfStars; i++) {
+        const star = {
+          x: Math.random() * window.innerWidth,
+          y: Math.random() * window.innerHeight,
+          size: Math.random() * 5 + 1,
+          speed: Math.random() * 3 + 2,
+          color: colors[Math.floor(Math.random() * colors.length)],
+        };
+        this.stars.push(star);
+      }
+    },
+    animateStars() {
+      const drawStars = () => {
+        const container = document.getElementById('starfield-container');
+        container.innerHTML = ''; // Clear previous stars
 
-      // Randomly assign color from the global colors
-      const color = colors[Math.floor(Math.random() * colors.length)];
-      star.style.backgroundColor = color;
+        this.stars.forEach((star) => {
+          const starElement = document.createElement('div');
+          starElement.className = 'star';
+          starElement.style.backgroundColor = star.color;
+          starElement.style.width = `${star.size}px`;
+          starElement.style.height = `${star.size}px`;
+          starElement.style.left = `${star.x}px`;
+          starElement.style.top = `${star.y}px`;
 
-      // Randomly assign size
-      const size = Math.random() * 5 + 1; // Increased size range
-      star.style.width = `${size}px`;
-      star.style.height = `${size}px`;
+          container.appendChild(starElement);
 
-      // Randomly assign position
-      star.style.left = `${Math.random() * 100}vw`;
-      star.style.top = `${Math.random() * 100}vh`;
+          // Update star position
+          star.y += star.speed;
+          if (star.y > window.innerHeight) {
+            star.y = 0; // Reset to top
+          }
+        });
 
-      // Randomly assign animation duration
-      const speed = Math.random() * 3 + 2;
-      star.style.animationDuration = `${speed}s`;
+        requestAnimationFrame(drawStars);
+      };
 
-      // Randomly assign hue and glow
-      const hue = Math.random();
-      const glow = Math.random() * 3;
-      star.style.setProperty('--hue', hue);
-      star.style.setProperty('--glow', glow);
-
-      container.appendChild(star);
-    }
+      drawStars();
+    },
   },
 };
 </script>
@@ -56,18 +75,8 @@ export default {
 .star {
   position: absolute;
   opacity: 0.8;
-  animation: moveStar linear infinite;
   border-radius: 50%;
-  filter: brightness(150%) hue-rotate(calc(360deg * var(--hue))) blur(2px);
-  box-shadow: 0 0 calc(5px + var(--glow) * 2px) rgba(255, 255, 255, 0.7);
-}
-
-@keyframes moveStar {
-  from {
-    transform: translateY(0px);
-  }
-  to {
-    transform: translateY(100vh);
-  }
+  box-shadow: 0 0 5px rgba(255, 255, 255, 0.5);
+  filter: hue-rotate(120deg) blur(2px);
 }
 </style>
